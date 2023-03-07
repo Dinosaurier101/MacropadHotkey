7a507dfd0b2b236456f4c7fb69fcc17712fbbe0a
Dinosaurier101 <dinosaurier@posteo.de>
2023-03-07 14:12:02 +0100

    added comments; removed print statements

--- code.py
+++ code.py
@@ -11,13 +11,15 @@ from adafruit_display_text import label
 from keyboard_layout_win_de import KeyboardLayout
 from keycode_win_de import Keycode
 
-# user defined variables
-pressed_color = 0xffffff
-MACRO_FOLDER = '/macros'
-brightness = 0.1
 
-# functions
-def set_pixels(keynum):      # set pixel to background color
+### user defined variables
+pressed_color = 0xffffff    # led color of the pressed key
+MACRO_FOLDER = '/macros'    # folder where macro files are stored
+brightness = 0.1            # overall led pixel brightness
+
+
+### functions
+def set_pixels(keynum):     # set pixel to background color. -1 for all.
     if keynum < 0:
         i = 0
         while i < 12:
@@ -85,8 +87,6 @@ def keypress(keynum):       # handle key presses
             #print(item)
             exec(item)
 
-
-
 def keyrelease(keynum):     # handle key releases
 
     set_pixels(keynum)
@@ -103,8 +103,7 @@ def keyrelease(keynum):     # handle key releases
                 macropad.stop_tone()
     macropad.consumer_control.release()
 
-
-def set_brightness():
+def set_brightness():       # set brightness of pixels and handle stealth mode
     global last_brightness
     global brightness
     last_brightness = brightness
@@ -118,33 +117,30 @@ def set_brightness():
         macropad.pixels.brightness = brightness
     macropad.pixels.show()
 
-def switch():
-    global app_name
-    # read colors, labels, keys, functions from apps
+def switch():               # handle switching between apps 
+    global app_name         # global to make it accessable for use in macro-functions
+    # clear and then read colors, labels, keys, functions from apps
     colors.clear()
     labels.clear()
     keys.clear()
     funcs.clear()
 
-    app_name = apps[app_num]['name']
+    app_name = apps[app_num]['name']    # app name as displayed on the top row
 
     a = 0
-    while a < 12:
-        #print('a', a)
-        #print(app_num)
-        #print(app['macros'][a][2])
-        colors.append(apps[app_num]['macros'][a][0])
+    while a < 12:   # split apps[] into smaller, more managable pieces
+        colors.append(apps[app_num]['macros'][a][0])    # explanation for apps[]... see 'initialisation' section
         labels.append(apps[app_num]['macros'][a][1])
         keys.append(apps[app_num]['macros'][a][2])
         try:
             funcs.append(apps[app_num]['macros'][a][3])
-        except:
+        except:     # enables compatibility with macros without functions (like the original Hotkeys firmware from Adafruit)
             funcs.append('')
-            # print('function missing in:', app_name)
         a = a + 1
 
-    # set colors, labels
+    # set pixel colors, labels
     set_pixels(-1)
+
     group[13].text = app_name
     for i in range(12):
         group[i].text = labels[i]
@@ -155,65 +151,67 @@ def switch():
     macropad.stop_tone()
     macropad.display.refresh()
 
+def import_apps():          # import macros from 'MACRO_FOLDER'
+    apps.clear()
+    files = os.listdir(MACRO_FOLDER)
+    files.sort()
+    for filename in files:
+        if filename.endswith('.py') and not filename.startswith('._'):
+            try:    #read all macros from their files and store in 'apps'
+                module = __import__(MACRO_FOLDER + '/' + filename[:-3])
+                apps.append(module.app)     # module.app is the dict containing the macro
+            except (SyntaxError, ImportError, AttributeError, KeyError, NameError,
+                    IndexError, TypeError) as err:
+                print("ERROR in", filename)
+                import traceback
+                traceback.print_exception(err, err, err.__traceback__)
+    if not apps:
+        group[13].text = 'NO MACRO FILES FOUND'
+        macropad.display.refresh()
+        while True:
+            pass
+
+def display_setup():        # set up display groups
+    group.clear()
+    group = displayio.Group()
+    for key_index in range(12):
+        x = key_index % 3
+        y = key_index // 3
+        group.append(label.Label(terminalio.FONT, text='', color=0xFFFFFF,
+                                anchored_position=((macropad.display.width - 1) * x / 2,
+                                                    macropad.display.height - 1 -
+                                                    (3 - y) * 12),
+                                anchor_point=(x / 2, 1.0)))
+    group.append(Rect(0, 0, macropad.display.width, 12, fill=0xFFFFFF))
+    group.append(label.Label(terminalio.FONT, text='', color=0x000000,
+                            anchored_position=(macropad.display.width//2, -2),
+                            anchor_point=(0.5, 0.0)))
+    macropad.display.show(group)
 
 
-# Initialisation
+### initialisation
 macropad = MacroPad(rotation=0, layout_class=KeyboardLayout, keycode_class=Keycode)
-macropad.display.auto_refresh = False
+macropad.display.auto_refresh = False   # don't automatically update screen to prevent flickering
 macropad.pixels.auto_write = False
-dim = 0
-app_num = 0
 
-print("\nMacroPad:")
+dim = 0         # dim = 1 allows to disable key illumination. set by encoder button.
+app_num = 0     # currently selected app/macro
 
-# import macros
-apps = []
-app_name = ()
-colors = []
-labels = []
-keys = []
-funcs = []
-
-files = os.listdir(MACRO_FOLDER)
-files.sort()
-for filename in files:
-    if filename.endswith('.py') and not filename.startswith('._'):
-        try:
-            module = __import__(MACRO_FOLDER + '/' + filename[:-3])     #read all macros from their files and store in 'apps'
-            # print('module:',module)
-            apps.append(module.app)
-        except (SyntaxError, ImportError, AttributeError, KeyError, NameError,
-                IndexError, TypeError) as err:
-            print("ERROR in", filename)
-            import traceback
-            traceback.print_exception(err, err, err.__traceback__)
-if not apps:
-    group[13].text = 'NO MACRO FILES FOUND'
-    macropad.display.refresh()
-    while True:
-        pass
-
-# set up display
-group = displayio.Group()
-for key_index in range(12):
-    x = key_index % 3
-    y = key_index // 3
-    group.append(label.Label(terminalio.FONT, text='', color=0xFFFFFF,
-                             anchored_position=((macropad.display.width - 1) * x / 2,
-                                                macropad.display.height - 1 -
-                                                (3 - y) * 12),
-                             anchor_point=(x / 2, 1.0)))
-group.append(Rect(0, 0, macropad.display.width, 12, fill=0xFFFFFF))
-group.append(label.Label(terminalio.FONT, text='', color=0x000000,
-                         anchored_position=(macropad.display.width//2, -2),
-                         anchor_point=(0.5, 0.0)))
-macropad.display.show(group)
+apps = []           # list to store all macros. to access: apps[app number from import]['macros'][key number][0:colors, 1:labels, 2:keys, 3:funcs]
+app_name = ()       # variable to store the currently loaded macro name
+colors = []         # list to store the pixel colors of the selected macro
+labels = []         # list to store the key labels of the selected macro
+keys = []           # list to store the key sequences of the selected macro
+funcs = []          # list to store the functions of the selected macro
 
 
-#apps[app number from import]['macros'][key number][0:colors, 1:labels, 2:keys, 3:funcs]
+import_apps()
+
+display_setup()
+
 set_brightness()
+
 switch()
-#macropad.display.show()
 
 
 # Main Loop
@@ -227,31 +225,25 @@ while True:
     else:
         macropad.red_led = False
 
-    # set dim value
+    # switch dim when encoder button is pressed
     macropad.encoder_switch_debounced.update()
     if macropad.encoder_switch_debounced.pressed:
         if dim == 0:
             dim = 1
-            print("dim 1")
         elif dim == 1:
             dim = 0
-            print("dim 0")
 
-
+    # switch selected app/macro when encoder position changes
     app_num_last = app_num
     app_num = macropad.encoder % len(apps)
-    #print(app_num)
     if app_num != app_num_last:
         switch()
-        #print('app switched')
 
+    # handle key presses
     key_event = macropad.keys.events.get()
     if key_event:
         if key_event.pressed:
             keypress(key_event.key_number)
 
-
         if key_event.released:
             keyrelease(key_event.key_number)
-
-
