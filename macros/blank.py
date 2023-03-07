db17fd7a886e0b0ebc9f624504d50e3dde22a854
Dinosaurier101 <dinosaurier@posteo.de>
2023-03-07 16:58:22 +0100

    updated blank.py

--- macros/blank.py
+++ macros/blank.py
@@ -1,6 +1,8 @@
 #from adafruit_hid.keycode import Keycode # REQUIRED if using Keycode.* values  # use this for QWERTY
 #from keycode_win_de import Keycode                                             # use this for QWERTZ
+#from keycode_win_fr import Keycode                                             # use this for AZERTY
 #from adafruit_hid.consumer_control_code import ConsumerControlCode
+#from adafruit_hid.mouse import Mouse
 
 app = {                       
     'name' : 'Empty Macro',    
